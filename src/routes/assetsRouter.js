a1027f0215735b2ef879c37f5c69612f1913f5ee
Kowshik M <157811305+Kowshikm0802@users.noreply.github.com>
2025-09-23 12:15:44 +0530

    add sever and laptop

--- src/routes/assetsRouter.js
+++ src/routes/assetsRouter.js
@@ -1,4 +1,4 @@
-const {getAssets,getStaff,addLaptops,toggleLaptopStatus,getAllLaptops}=require('../controllers/assetsController');
+const {getAssets,getStaff,addLaptops,toggleLaptopStatus,updateServer,getAllLaptops,getAllDesktop,getAllServer,updateLaptops,addDesktop,addServer,toggleDesktopStatus,toggleServerStatus}=require('../controllers/assetsController');
 const express=require('express');
 const assetsRouter=express.Router();
 
@@ -6,6 +6,19 @@ const assetsRouter=express.Router();
 assetsRouter.get('/Laptops',getAllLaptops);
 assetsRouter.post('/Laptops/add',addLaptops);
 assetsRouter.put('/Laptops/status/:id',toggleLaptopStatus);
+assetsRouter.put('/Laptops/update',updateLaptops);
+
+
+// desktop
+assetsRouter.get('/Desktops',getAllDesktop);
+assetsRouter.post('/Desktops/add',addDesktop);
+assetsRouter.put('/Desktops/status/:id',toggleDesktopStatus);
+// server
+assetsRouter.get('/Servers',getAllServer);
+assetsRouter.post('/Servers/add',addServer);
+assetsRouter.put('/Servers/status/:id',toggleServerStatus);
+assetsRouter.put('/Servers/update',updateServer);
+
 
 assetsRouter.get('/getstaff',getStaff);
 assetsRouter.get('/:id',getAssets);
