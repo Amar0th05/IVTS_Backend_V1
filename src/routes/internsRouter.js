b3d6159f6865e2121e313c843c9556a1c5194be2
vasangk <vasangk24@gmail.com>
2025-09-25 10:49:42 +0530

    internrouter updated interndetails

--- src/routes/internsRouter.js
+++ src/routes/internsRouter.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const multer = require('multer');
-const { getAllIntern, getInternById, getMetadata, downloadDocument,deleteDocument,uploadDocument,updateinternDetails,toggleInternStatus,createIntern} = require('../controllers/internsController');
+const {
+  getAllIntern,
+  getInternById,
+  getMetadata,
+  downloadDocument,
+  deleteDocument,
+  uploadDocument,
+  updateinternDetails,
+  toggleInternStatus,
+  createIntern
+} = require('../controllers/internsController');
 
 const internsRouter = express.Router();
 
-// Use memory storage (you can switch to diskStorage if you want to save files physically)
+// Multer in memory storage
 const upload = multer({ storage: multer.memoryStorage() });
 
-
+// Apply internship
 internsRouter.post(
   '/apply',
   upload.fields([
@@ -19,26 +29,25 @@ internsRouter.post(
   createIntern
 );
 
-internsRouter.post(
-  '/apply',
+// Update intern details (including files if uploaded)
+internsRouter.put(
+  '/:id',
   upload.fields([
     { name: 'resume', maxCount: 1 },
     { name: 'photo', maxCount: 1 },
     { name: 'idProof', maxCount: 1 },
-    { name: 'bonafide', maxCount: 1 },
+    { name: 'bonafide', maxCount: 1 }
   ]),
-  createIntern
+  updateinternDetails
 );
-internsRouter.put('/:id',updateinternDetails);
-internsRouter.get('/all',getAllIntern);
-internsRouter.get('/:id', getInternById);
+
+// Other routes
+internsRouter.get('/all', getAllIntern);
 internsRouter.get('/:id/documents/metadata', getMetadata);
 internsRouter.get('/:internId/documents/:docName', downloadDocument);
 internsRouter.delete('/:internId/documents/:docName', deleteDocument);
 internsRouter.post('/:internId/documents/:docName', upload.single('file'), uploadDocument);
-internsRouter.put('/status/:id',toggleInternStatus);
+internsRouter.put('/status/:id', toggleInternStatus);
+internsRouter.get('/:id', getInternById);
 
-
-
-
-module.exports = {internsRouter} ;
+module.exports = { internsRouter };
